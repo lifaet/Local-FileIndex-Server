82a8d47ae1e7091502056dcf94b4952f3996102f
Zim <lifaethossain@gmail.com>
2025-07-07 17:56:41 +0600

    0.1.8

--- app.py
+++ app.py
@@ -143,4 +143,4 @@ def serve_files(path):
     return send_from_directory('static', 'index.html')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
